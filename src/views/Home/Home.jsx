1838c1f54b3b3a3c7bd2242d7d8197f8575df8e7
MRaehanR <raehanrobban2@gmail.com>
2023-08-15 01:52:52 +0700

    feat: search video

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, FormControl, Input } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import CardVideo from "../../components/CardVideo/CardVideo";
 import CategoryList from "../../components/CategoryList/CategoryList";
@@ -7,6 +7,16 @@ import CategoryList from "../../components/CategoryList/CategoryList";
 function Home() {
   const navigate = useNavigate();
   const [videos, setVideos] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const handleInputSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchVideos();
+  };
 
   const clickHandle = ({ id, videoUrl }) => {
     navigate("/detail", {
@@ -23,9 +33,10 @@ function Home() {
 
   const fetchVideos = async () => {
     try {
-      const response = await fetch(
-        `${process.env.REACT_APP_SERVER_URL}/videos`
-      );
+      const url = search
+        ? `${process.env.REACT_APP_SERVER_URL}/videos?search=${search}`
+        : `${process.env.REACT_APP_SERVER_URL}/videos`;
+      const response = await fetch(url);
       const data = await response.json();
       setVideos(data.data);
     } catch (error) {
@@ -39,6 +50,21 @@ function Home() {
 
   return (
     <Box padding={3}>
+      <Box h="20%" w="100%" padding={3}>
+        <form onSubmit={handleSubmit}>
+          <Input
+            variant="outline"
+            placeholder="Search"
+            color="white"
+            marginBottom={2}
+            marginTop={3}
+            size="md"
+            borderRadius={5}
+            type="text"
+            onChange={handleInputSearch}
+          />
+        </form>
+      </Box>
       <Box marginTop={5}>
         <CategoryList getData={getDataFromCategoryList} />
       </Box>
